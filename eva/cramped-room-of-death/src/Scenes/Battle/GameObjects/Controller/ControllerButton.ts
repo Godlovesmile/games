2c2ee4defd9fac98bc9ee312cf27f681c25c2436
lizhi <15501251520@163.com>
2022-12-19 14:30:37 +0800

    feat: 按钮操作事件 + 动画

--- eva/cramped-room-of-death/src/Scenes/Battle/GameObjects/Controller/ControllerButton.ts
+++ eva/cramped-room-of-death/src/Scenes/Battle/GameObjects/Controller/ControllerButton.ts
@@ -1,6 +1,8 @@
 import { GameObject } from '@eva/eva.js'
 // import { Img } from '@eva/plugin-renderer-img'
 import { Sprite } from '@eva/plugin-renderer-sprite'
+import { Event } from '@eva/plugin-renderer-event'
+import { Transition } from '@eva/plugin-transition'
 
 const CTRL_WIDTH = 70
 const CTRL_HEIGHT = 60
@@ -36,6 +38,92 @@ const ControllerButton = (index: number) => {
       spriteName: `ctrl (${index}).png`,
     })
   )
+
+  // 添加动画
+  const animation = button.addComponent(new Transition())
+  animation.group = {
+    small: [
+      {
+        name: 'scale.x',
+        component: button.transform,
+        values: [
+          {
+            time: 0,
+            value: 1,
+            tween: 'ease-out',
+          },
+          {
+            time: 100,
+            value: 0.9,
+            tween: 'ease-in',
+          },
+        ],
+      },
+      {
+        name: 'scale.y',
+        component: button.transform,
+        values: [
+          {
+            time: 0,
+            value: 1,
+            tween: 'ease-out',
+          },
+          {
+            time: 100,
+            value: 0.9,
+            tween: 'ease-in',
+          },
+        ],
+      },
+    ],
+    big: [
+      {
+        name: 'scale.x',
+        component: button.transform,
+        values: [
+          {
+            time: 0,
+            value: 0.9,
+            tween: 'ease-out',
+          },
+          {
+            time: 100,
+            value: 1,
+            tween: 'ease-in',
+          },
+        ],
+      },
+      {
+        name: 'scale.y',
+        component: button.transform,
+        values: [
+          {
+            time: 0,
+            value: 0.9,
+            tween: 'ease-out',
+          },
+          {
+            time: 100,
+            value: 1,
+            tween: 'ease-in',
+          },
+        ],
+      },
+    ],
+  }
+
+  // 添加事件
+  const event = button.addComponent(new Event())
+  const endHandler = () => {
+    console.log('=== touchend ===')
+    animation.play('big')
+  }
+
+  event.on('touchstart', () => {
+    animation.play('small')
+  })
+  event.on('touchend', endHandler)
+  event.on('touchendoutside', endHandler)
 
   return button
 }
